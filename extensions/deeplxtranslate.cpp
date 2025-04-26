579539a54bfa8393ebafd17f2b5aa26cb2bb9c0c
Chenx221 <chenx221@yandex.com>
2025-04-26 17:16:58 +0800

    feat(Deeplx): add support for DeepLX.

---
+++ extensions/deeplxtranslate.cpp
@@ -0,0 +1,199 @@
+﻿#include "qtcommon.h"
+#include "translatewrapperdeeplx.h"
+#include "network.h"
+
+extern const wchar_t *TRANSLATION_ERROR;
+
+const char *TRANSLATION_PROVIDER = "DeepLX Translate";
+const char *GET_API_KEY_FROM = "https://deeplx.owo.network/install/variables.html";
+
+extern const QStringList version{
+    "free", "v1", "v2"
+};
+extern const QStringList languagesTo
+        {
+            "Arabic",
+            "Bulgarian",
+            "Czech",
+            "Danish",
+            "German",
+            "Greek",
+            "English (backward compatibility)",
+            "English (British)",
+            "English (American)",
+            "Spanish",
+            "Estonian",
+            "Finnish",
+            "French",
+            "Hungarian",
+            "Indonesian",
+            "Italian",
+            "Japanese",
+            "Korean",
+            "Lithuanian",
+            "Latvian",
+            "Norwegian Bokm�l",
+            "Dutch",
+            "Polish",
+            "Portuguese (backward compatibility)",
+            "Portuguese (Brazilian)",
+            "Portuguese (all Portuguese variants excluding Brazilian Portuguese)",
+            "Romanian",
+            "Russian",
+            "Slovak",
+            "Slovenian",
+            "Swedish",
+            "Turkish",
+            "Ukrainian",
+            "Chinese (backward compatibility)",
+            "Chinese (simplified)",
+            "Chinese (traditional)"
+        },
+        languagesFrom
+        {
+            "Arabic",
+            "Bulgarian",
+            "Chinese (all Chinese variants)",
+            "Czech",
+            "Danish",
+            "Dutch",
+            "English (all English variants)",
+            "Estonian",
+            "Finnish",
+            "French",
+            "German",
+            "Greek",
+            "Hungarian",
+            "Indonesian",
+            "Italian",
+            "Japanese",
+            "Korean",
+            "Latvian",
+            "Lithuanian",
+            "Norwegian Bokm�l",
+            "Polish",
+            "Portuguese (all Portuguese variants)",
+            "Romanian",
+            "Russian",
+            "Slovak",
+            "Slovenian",
+            "Spanish",
+            "Swedish",
+            "Turkish",
+            "Ukrainian"
+        };
+extern const std::unordered_map<std::wstring, std::wstring> codes
+{
+    {{L"Arabic"}, {L"AR"}},
+    {{L"Bulgarian"}, {L"BG"}},
+    {{L"Czech"}, {L"CS"}},
+    {{L"Danish"}, {L"DA"}},
+    {{L"German"}, {L"DE"}},
+    {{L"Greek"}, {L"EL"}},
+    {{L"English (all English variants)"}, {L"EN"}},
+    {{L"English (backward compatibility)"}, {L"EN"}},
+    {{L"English (British)"}, {L"EN-GB"}},
+    {{L"English (American)"}, {L"EN-US"}},
+    {{L"Spanish"}, {L"ES"}},
+    {{L"Estonian"}, {L"ET"}},
+    {{L"Finnish"}, {L"FI"}},
+    {{L"French"}, {L"FR"}},
+    {{L"Hungarian"}, {L"HU"}},
+    {{L"Indonesian"}, {L"ID"}},
+    {{L"Italian"}, {L"IT"}},
+    {{L"Japanese"}, {L"JA"}},
+    {{L"Korean"}, {L"KO"}},
+    {{L"Lithuanian"}, {L"LT"}},
+    {{L"Latvian"}, {L"LV"}},
+    {{L"Norwegian Bokm�l"}, {L"NB"}},
+    {{L"Dutch"}, {L"NL"}},
+    {{L"Polish"}, {L"PL"}},
+    {{L"Portuguese (all Portuguese variants)"}, {L"PT"}},
+    {{L"Portuguese (backward compatibility)"}, {L"PT"}},
+    {{L"Portuguese (Brazilian)"}, {L"PT-BR"}},
+    {{L"Portuguese (all Portuguese variants excluding Brazilian Portuguese)"}, {L"PT-PT"}},
+    {{L"Romanian"}, {L"RO"}},
+    {{L"Russian"}, {L"RU"}},
+    {{L"Slovak"}, {L"SK"}},
+    {{L"Slovenian"}, {L"SL"}},
+    {{L"Swedish"}, {L"SV"}},
+    {{L"Turkish"}, {L"TR"}},
+    {{L"Ukrainian"}, {L"UK"}},
+    {{L"Chinese (all Chinese variants)"}, {L"ZH"}},
+    {{L"Chinese (backward compatibility)"}, {L"ZH"}},
+    {{L"Chinese (simplified)"}, {L"ZH-HANS"}},
+    {{L"Chinese (traditional)"}, {L"ZH-HANT"}},
+    {{L"?"}, {L"auto"}}
+
+};
+
+bool translateSelectedOnly = false, useRateLimiter = true, rateLimitSelected = false, useCache = true, useFilter = true;
+int tokenCount = 30, rateLimitTimespan = 60000, maxSentenceSize = 5000, port = 1188;
+
+std::wstring RemoveZeroWidthSpace(std::wstring text) {
+    text.erase(std::remove(text.begin(), text.end(), 0x200b), text.end());
+    return text;
+}
+
+std::pair<bool, std::wstring> Translate(const std::wstring &text, TranslationParam tlp) {
+    std::wstring cleanedText = RemoveZeroWidthSpace(text);
+    if (tlp.translateVersion == L"free") {
+        if (HttpRequest httpRequest{
+            L"Mozilla/5.0 Textractor",
+            L"127.0.0.1",
+            L"POST",
+            L"/translate",
+            FormatString(R"({"text":"%s","source_lang":"%S","target_lang":"%S"})",
+                         JSON::Escape(WideStringToString(cleanedText)), codes.at(tlp.translateFrom),
+                         codes.at(tlp.translateTo)),
+            FormatString(L"Content-Type: application/json; charset=UTF-8\r\nAuthorization: Bearer %s",
+                         tlp.authKey).c_str(),
+            DWORD(port),
+            NULL,
+            WINHTTP_FLAG_ESCAPE_DISABLE
+        }) {
+            if (auto translation = Copy(JSON::Parse(httpRequest.response)[L"data"].String()))
+                return {true, HTML::Unescape(translation.value())};
+            return {false, FormatString(L"%s: %s", TRANSLATION_ERROR, httpRequest.response)};
+        } else return {false, FormatString(L"%s (code=%u)", TRANSLATION_ERROR, httpRequest.errorCode)};
+    } else if (tlp.translateVersion == L"v1") {
+        if (HttpRequest httpRequest{
+            L"Mozilla/5.0 Textractor",
+            L"127.0.0.1",
+            L"POST",
+            L"/v1/translate",
+            FormatString(R"({"text":"%s","source_lang":"%S","target_lang":"%S"})",
+                         JSON::Escape(WideStringToString(text)), codes.at(tlp.translateFrom),
+                         codes.at(tlp.translateTo)),
+            FormatString(L"Content-Type: application/json; charset=UTF-8\r\nAuthorization: Bearer %s",
+                         tlp.authKey).c_str(),
+            DWORD(port),
+            NULL,
+            WINHTTP_FLAG_ESCAPE_DISABLE
+        }) {
+            if (auto translation = Copy(JSON::Parse(httpRequest.response)[L"data"].String()))
+                return {true, HTML::Unescape(translation.value())};
+            return {false, FormatString(L"%s: %s", TRANSLATION_ERROR, httpRequest.response)};
+        } else return {false, FormatString(L"%s (code=%u)", TRANSLATION_ERROR, httpRequest.errorCode)};
+    } else {
+        // v2
+        if (HttpRequest httpRequest{
+            L"Mozilla/5.0 Textractor",
+            L"127.0.0.1",
+            L"POST",
+            L"/v2/translate",
+            FormatString(R"({"text":["%s"],"source_lang":"%S","target_lang":"%S"})",
+                         JSON::Escape(WideStringToString(text)), codes.at(tlp.translateFrom),
+                         codes.at(tlp.translateTo)),
+            FormatString(L"Content-Type: application/json; charset=UTF-8\r\nAuthorization: DeepL-Auth-Key %s",
+                         tlp.authKey).c_str(),
+            DWORD(port),
+            NULL,
+            WINHTTP_FLAG_ESCAPE_DISABLE
+        }) {
+            if (auto translation = Copy(JSON::Parse(httpRequest.response)[L"translations"][0][L"text"].String()))
+                return {true, HTML::Unescape(translation.value())};
+            return {false, FormatString(L"%s: %s", TRANSLATION_ERROR, httpRequest.response)};
+        } else return {false, FormatString(L"%s (code=%u)", TRANSLATION_ERROR, httpRequest.errorCode)};
+    }
+}
---
+++ extensions/translatewrapperdeeplx.cpp
@@ -0,0 +1,252 @@
+﻿#include "qtcommon.h"
+#include "extension.h"
+#include "translatewrapperdeeplx.h"
+#include "blockmarkup.h"
+#include <concurrent_priority_queue.h>
+#include <fstream>
+#include <QComboBox>
+
+#include "../texthook/main.h"
+
+extern const char* NATIVE_LANGUAGE;
+extern const char* TRANSLATE_TO;
+extern const char* TRANSLATE_FROM;
+extern const char* TRANSLATE_SELECTED_THREAD_ONLY;
+extern const char* RATE_LIMIT_ALL_THREADS;
+extern const char* RATE_LIMIT_SELECTED_THREAD;
+extern const char* USE_TRANS_CACHE;
+extern const char* FILTER_GARBAGE;
+extern const char* MAX_TRANSLATIONS_IN_TIMESPAN;
+extern const char* TIMESPAN;
+extern const char* MAX_SENTENCE_SIZE;
+extern const char* API_KEY;
+extern const wchar_t* SENTENCE_TOO_LARGE_TO_TRANS;
+extern const wchar_t* TRANSLATION_ERROR;
+extern const wchar_t* TOO_MANY_TRANS_REQUESTS;
+
+extern const char* TRANSLATION_PROVIDER;
+extern const char* GET_API_KEY_FROM;
+extern const QStringList languagesTo, languagesFrom, version;
+extern bool translateSelectedOnly, useRateLimiter, rateLimitSelected, useCache, useFilter;
+extern int tokenCount, rateLimitTimespan, maxSentenceSize, port;
+
+const char* TRANSLATE_VERSION = u8"Version";
+
+std::pair<bool, std::wstring> Translate(const std::wstring& text, TranslationParam tlp);
+
+QFormLayout* display;
+Settings settings;
+
+namespace
+{
+	Synchronized<TranslationParam> tlp;
+	Synchronized<std::unordered_map<std::wstring, std::wstring>> translationCache;
+
+	std::string CacheFile()
+	{
+		return FormatString("%s Cache (%S).txt", TRANSLATION_PROVIDER, tlp->translateTo);
+	}
+	void SaveCache()
+	{
+		std::wstring allTranslations(L"\xfeff");
+		for (const auto& [sentence, translation] : translationCache.Acquire().contents)
+			allTranslations.append(L"|SENTENCE|").append(sentence).append(L"|TRANSLATION|").append(translation).append(L"|END|\r\n");
+		std::ofstream(CacheFile(), std::ios::binary | std::ios::trunc).write((const char*)allTranslations.c_str(), allTranslations.size() * sizeof(wchar_t));
+	}
+	void LoadCache()
+	{
+		translationCache->clear();
+		std::ifstream stream(CacheFile(), std::ios::binary);
+		BlockMarkupIterator savedTranslations(stream, Array<std::wstring_view>{ L"|SENTENCE|", L"|TRANSLATION|" });
+		auto translationCache = ::translationCache.Acquire();
+		while (auto read = savedTranslations.Next())
+		{
+			auto& [sentence, translation] = read.value();
+			translationCache->try_emplace(std::move(sentence), std::move(translation));
+		}
+	}
+}
+
+class Window : public QDialog, Localizer
+{
+public:
+	Window() : QDialog(nullptr, Qt::WindowMinMaxButtonsHint)
+	{
+		display = new QFormLayout(this);
+
+		settings.beginGroup(TRANSLATION_PROVIDER);
+
+		auto translateToCombo = new QComboBox(this);
+		translateToCombo->addItems(languagesTo);
+		int i = -1;
+		if (settings.contains(TRANSLATE_TO)) i = translateToCombo->findText(settings.value(TRANSLATE_TO).toString());
+		if (i < 0) i = translateToCombo->findText(NATIVE_LANGUAGE, Qt::MatchStartsWith);
+		if (i < 0) i = translateToCombo->findText("English", Qt::MatchStartsWith);
+		translateToCombo->setCurrentIndex(i);
+		SaveTranslateTo(translateToCombo->currentText());
+		display->addRow(TRANSLATE_TO, translateToCombo);
+		connect(translateToCombo, &QComboBox::currentTextChanged, this, &Window::SaveTranslateTo);
+
+		auto translateFromCombo = new QComboBox(this);
+		translateFromCombo->addItem("?");
+		translateFromCombo->addItems(languagesFrom);
+		i = -1;
+		if (settings.contains(TRANSLATE_FROM)) i = translateFromCombo->findText(settings.value(TRANSLATE_FROM).toString());
+		if (i < 0) i = 0;
+		translateFromCombo->setCurrentIndex(i);
+		SaveTranslateFrom(translateFromCombo->currentText());
+		display->addRow(TRANSLATE_FROM, translateFromCombo);
+		connect(translateFromCombo, &QComboBox::currentTextChanged, this, &Window::SaveTranslateFrom);
+
+		auto translateVersionCombo = new QComboBox(this);
+		translateVersionCombo->addItems(version);
+		i = -1;
+		if (settings.contains(TRANSLATE_VERSION)) i = translateVersionCombo->findText(settings.value(TRANSLATE_VERSION).toString());
+		if (i < 0) i = 0;
+		translateVersionCombo->setCurrentIndex(i);
+		SaveTranslateVersion(translateVersionCombo->currentText());
+		display->addRow(TRANSLATE_VERSION, translateVersionCombo);
+		connect(translateVersionCombo, &QComboBox::currentTextChanged, this, &Window::SaveTranslateVersion);
+
+		for (auto [value, label] : Array<bool&, const char*>{
+			{ translateSelectedOnly, TRANSLATE_SELECTED_THREAD_ONLY },
+			{ useRateLimiter, RATE_LIMIT_ALL_THREADS },
+			{ rateLimitSelected, RATE_LIMIT_SELECTED_THREAD },
+			{ useCache, USE_TRANS_CACHE },
+			{ useFilter, FILTER_GARBAGE }
+		})
+		{
+			value = settings.value(label, value).toBool();
+			auto checkBox = new QCheckBox(this);
+			checkBox->setChecked(value);
+			display->addRow(label, checkBox);
+			connect(checkBox, &QCheckBox::clicked, [label, &value](bool checked) { settings.setValue(label, value = checked); });
+		}
+		for (auto [value, label] : Array<int&, const char*>{
+			{ tokenCount, MAX_TRANSLATIONS_IN_TIMESPAN },
+			{ rateLimitTimespan, TIMESPAN },
+			{ maxSentenceSize, MAX_SENTENCE_SIZE },
+{port, "Port"}
+		})
+		{
+			value = settings.value(label, value).toInt();
+			auto spinBox = new QSpinBox(this);
+			spinBox->setRange(0, INT_MAX);
+			if (label == "Port") spinBox->setRange(1, 65535);
+			spinBox->setValue(value);
+			display->addRow(label, spinBox);
+			connect(spinBox, qOverload<int>(&QSpinBox::valueChanged), [label, &value](int newValue) { settings.setValue(label, value = newValue); });
+		}
+		if (GET_API_KEY_FROM)
+		{
+			auto keyEdit = new QLineEdit(settings.value(API_KEY).toString(), this);
+			tlp->authKey = S(keyEdit->text());
+			QObject::connect(keyEdit, &QLineEdit::textChanged, [](QString key) { settings.setValue(API_KEY, S(tlp->authKey = S(key))); });
+			auto keyLabel = new QLabel(QString("<a href=\"%1\">%2</a>").arg(GET_API_KEY_FROM, API_KEY), this);
+			keyLabel->setOpenExternalLinks(true);
+			display->addRow(keyLabel, keyEdit);
+		}
+
+		setWindowTitle(TRANSLATION_PROVIDER);
+		QMetaObject::invokeMethod(this, &QWidget::show, Qt::QueuedConnection);
+	}
+
+	~Window()
+	{
+		SaveCache();
+	}
+
+private:
+	void SaveTranslateTo(QString language)
+	{
+		SaveCache();
+		settings.setValue(TRANSLATE_TO, S(tlp->translateTo = S(language)));
+		LoadCache();
+	}
+	void SaveTranslateFrom(QString language)
+	{
+		settings.setValue(TRANSLATE_FROM, S(tlp->translateFrom = S(language)));
+	}
+	void SaveTranslateVersion(QString version)
+	{
+		settings.setValue(TRANSLATE_VERSION, S(tlp->translateVersion = S(version)));
+	}
+} window;
+
+std::vector<std::wstring> splitWString(const std::wstring& sentence, const std::wstring& delimiter) {
+	std::vector<std::wstring> result;
+	size_t start = 0;
+	size_t end = sentence.find(delimiter);
+
+	while (end != std::wstring::npos) {
+		result.push_back(sentence.substr(start, end - start));
+		start = end + delimiter.length();
+		end = sentence.find(delimiter, start);
+	}
+
+	result.push_back(sentence.substr(start));
+	return result;
+}
+
+bool ProcessSentence(std::wstring &sentence, SentenceInfo sentenceInfo) {
+    if (sentenceInfo["text number"] == 0) return false; // not processing the sentence from console
+
+    static class {
+    public:
+        bool Request() {
+            DWORD64 current = GetTickCount64(), token;
+            while (tokens.try_pop(token))
+                if (token > current - rateLimitTimespan) {
+                    tokens.push(token); // popped one too many
+                    break;
+                }
+            bool available = tokens.size() < tokenCount;
+            if (available) tokens.push(current);
+            return available;
+        }
+
+    private:
+        concurrency::concurrent_priority_queue<DWORD64, std::greater<DWORD64> > tokens;
+    } rateLimiter;
+
+    bool cache = false;
+    std::wstring translation;
+    std::vector<std::wstring> parts = splitWString(sentence, L"\x200b \n");
+    std::wstring &originalSentence = parts[0];
+
+    if (useFilter) {
+        Trim(originalSentence);
+        originalSentence.erase(std::remove_if(originalSentence.begin(), originalSentence.end(),
+                                              [](wchar_t ch) { return ch < ' ' && ch != '\n'; }), originalSentence.end());
+    }
+    if (originalSentence.empty()) return true;
+    if (originalSentence.size() > maxSentenceSize) translation = SENTENCE_TOO_LARGE_TO_TRANS;
+    if (useCache) {
+        auto translationCache = ::translationCache.Acquire();
+        if (auto it = translationCache->find(originalSentence); it != translationCache->end()) translation = it->second;
+    }
+    if (translation.empty() && (!translateSelectedOnly || sentenceInfo["current select"]))
+        if (rateLimiter.Request() || !useRateLimiter || (!rateLimitSelected && sentenceInfo["current select"]))
+            std::tie(cache, translation) = Translate(originalSentence, tlp.Copy());
+        else translation = TOO_MANY_TRANS_REQUESTS;
+    if (cache) translationCache->operator[](originalSentence) = translation;
+
+    if (useFilter) Trim(translation);
+    for (int i = 0; i < translation.size(); ++i) if (translation[i] == '\r' && translation[i + 1] == '\n')
+        translation[i] = 0x200b; // for some reason \r appears as newline - no need to double
+    if (translation.empty()) translation = TRANSLATION_ERROR;
+
+    sentence = originalSentence;
+    for (size_t i = 1; i < parts.size(); ++i) {
+        sentence += L"\x200b \n" + parts[i];
+    }
+    sentence += L"\x200b \n" + translation;
+    return true;
+}
+
+extern const std::unordered_map<std::wstring, std::wstring> codes;
+TEST(
+	{
+		assert(Translate(L"こんにちは", { L"English (American)", L"?", L"free", L"" }).second.find(L"ello") == 1 || strstr(TRANSLATION_PROVIDER, "DevTools"));
+	}
+);

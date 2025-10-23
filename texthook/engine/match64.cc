cb72adf6d8e981daaa5964837c9e5aa456852a69
Chenx221 <chenx221@yandex.com>
2025-10-23 20:50:51 +0800

    feat(BGI64Ex): Improve BGI64Ex hook filtering conditions

--- texthook/engine/match64.cc
+++ texthook/engine/match64.cc
@@ -1396,7 +1396,7 @@ namespace Engine
 				auto text = reinterpret_cast<char*>(str);
 				size_t len = *size;
 
-				std::regex pattern(R"(<r.*?>|<\/r>)");
+				std::regex pattern(R"(<r.*?>|<\/r>|<FS.*?>)");
 				RegexReplacer(text, &len, pattern, "");
 				*size = len;
 				return true;
